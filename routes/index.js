99083d3b877c97dec648c2ef8702ef3a72eb1c92
nik19ta <nikhvatov19@gmail.com>
2020-09-30 12:45:53 +0300

    add rederect

--- routes/index.js
+++ routes/index.js
@@ -8,30 +8,30 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json()
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'VzCxia9LItze',
-    database: 'croc'
-});
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'VzCxia9LItze',
+//     database: 'croc'
+// });
 
-function login(login, password){
-    connection.query('SELECT * FROM users where login = login and password = password', (err, data, fields)=> {
-        if (data.length > 0) {
-            return "success"
-        } else {
-            return "error"
-        }
-    });
-};
-function reg(login, password) {
-    connection.query('INSERT INTO users values (login, password)', (err, data, fields) => {
-        if (err) {
-            return "error"
-        } else {
-            return "success"
-        }
-    })}
+// function login(login, password){
+//     connection.query('SELECT * FROM users where login = login and password = password', (err, data, fields)=> {
+//         if (data.length > 0) {
+//             return "success"
+//         } else {
+//             return "error"
+//         }
+//     });
+// };
+// function reg(login, password) {
+//     connection.query('INSERT INTO users values (login, password)', (err, data, fields) => {
+//         if (err) {
+//             return "error"
+//         } else {
+//             return "success"
+//         }
+//     })}
 
 
 
@@ -55,10 +55,10 @@ router.get('/', function(req, res) {
 router.get('/redirect', function(req, res) {
     res.sendFile(path.resolve(html + '/redir.html'));
 
-    let parameters = {crap1: 'toilet', crap2: 'handywash'};
-    let finalURL = createURLwithParameters(baseURL, parameters);
-    console.log("final URL is ", finalURL);
-    res.redirect(finalURL)
+    // let parameters = {crap1: 'toilet', crap2: 'handywash'};
+    // let finalURL = createURLwithParameters(baseURL, parameters);
+    // console.log("final URL is ", finalURL);
+    // res.redirect(finalURL)
 });
 
 
@@ -77,10 +77,13 @@ router.get('/form', function (req, res) {
 
 })
 
-    router.post('/getForm', jsonParser, function (req, res) {
-        console.log(req.body.login, req.body.password);
+router.post('/getForm', jsonParser, function (req, res) {
+    console.log(req.body.login, req.body.password);
         
-    } )
+} )
+router.post('/checkParams', jsonParser, function (req, res) {
+    console.log(`Человек перешёл по ссылки у которой параметры ${req.body.param1}, ${req.body.param2}`);
+} )
 
 
 
